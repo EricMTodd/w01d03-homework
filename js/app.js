85c032ddda95dc01e71bc6904ef6985a0f466763
Eric M Todd <ericmtodd@Erics-MacBook-Air.local>
2018-06-13 18:58:41 -0600

    Commit 2 - Get Even answered.

--- js/app.js
+++ js/app.js
@@ -4,3 +4,10 @@ console.log("JavaScript is running...");
 for (let i = 1; i <= 20; i++) {
 	console.log(i)
 };
+
+// For loop that logs even numbers 0 - 200.
+for (let i = 0; i <= 200; i++) {
+	if (i % 2 === 0) {
+		console.log(i)
+	}
+};
