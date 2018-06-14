914fa0607d84499491c92fc91c771d29d1883627
Eric M Todd <ericmtodd@Erics-MacBook-Air.local>
2018-06-13 21:00:32 -0600

    Commit 5 - Getting to know you answered.

--- js/app.js
+++ js/app.js
@@ -43,5 +43,16 @@ for (i = 1; i <= 100; i++) {
     }
 };
 
+//Getting to know you
+const thom = ["Thom", 1000, "Christchurch"]
+const karolin = ["Karolin", 16, "New York"]
+const kristyn = ["Kristyn", 5, "Pittsburgh"]
+const matt = ["Matt H", 186, "Philadelphia"]
+
+thom[0] = "Gameboy"
+karolin[1] = 17
+matt[2] = "Gotham City"
+kristyn.pop();
+kristyn.push("Brooklyn");
 
 
