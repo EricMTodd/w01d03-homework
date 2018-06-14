74006c8d5c6a5cfd6f851802c3272ec8410359d7
Eric M Todd <ericmtodd@Erics-MacBook-Air.local>
2018-06-13 19:54:53 -0600

    Commit 3 - Excited Kittens answered.

--- js/app.js
+++ js/app.js
@@ -2,12 +2,29 @@ console.log("JavaScript is running...");
 
 // Counts from 1 to 20.
 for (let i = 1; i <= 20; i++) {
-	console.log(i)
+	console.log(i);
 };
 
 // For loop that logs even numbers 0 - 200.
 for (let i = 0; i <= 200; i++) {
 	if (i % 2 === 0) {
-		console.log(i)
+		console.log(i);
 	}
+};
+
+// Excited Kitten
+
+// Love me, pet me! HSSSSSS!
+
+for (let i = 1; i <= 20; i++) {
+	console.log("Love me, pet me! HSSSSSS!");
+};
+
+let phrase = ["...human...why you taking pictures of me?...", "...the catnip made me do it...", "...why does the red dot always get away..."];
+let length = phrase.length;
+
+for (let i = 0; i <= 20; i++) {
+	let random = Math.floor(Math.random() * length);
+	if (i % 2 === 0)
+	console.log(phrase[random]);
 };
